d20a0613b2fe5666e05cf5d470b5763e342cc876
Perttu Yli-Opas <perttu.yo@gmail.com>
2017-01-26 15:03:37 +0200

    The beginning

--- src/parvi/Parvi.java
+++ src/parvi/Parvi.java
@@ -5,24 +5,54 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.Toolkit;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 
-public class Parvi extends JPanel{
+public class Parvi extends JPanel implements Runnable{
 
     private static Flock flock;
     public static void main(String[] args){
-        JFrame f = new JFrame("They fly (soon)!");
         
         JPanel p = new Parvi();
         p.setPreferredSize(new Dimension(800, 600));
         
+        JFrame f = new JFrame("They fly (soon)!");
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         f.add(p);
         f.pack();
+        
+        
+        //position frame to center of screen
+        Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
+        f.setBounds((int)(screen.getWidth() / 2 - f.getWidth() / 2), 
+            (int)(screen.getHeight() / 2 - f.getHeight() / 2), f.getWidth(), f.getHeight());
+        f.setVisible(true);
+        
         f.setVisible(true);
         
         flock = new Flock(20);
+    }
+    
+    @Override
+    public void run(){
+        double lastTime = getTime();
+        while(true){
+            
+            double newTime = getTime();
+            flock.runStep(newTime - lastTime);
+            lastTime = newTime;
+            try{
+                Thread.sleep(10);
+            }catch(InterruptedException e){
+                e.printStackTrace(System.err);
+            }
+        }
+    }
+    
+    private double getTime(){
+        return (double)System.nanoTime() / 1e9;
     }
     
     @Override
